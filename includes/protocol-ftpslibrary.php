b896a1ba34284ea9f62a9dfc82134c1cc29f4ead
Greg Ross <greg@toolstack.com>
2014-03-13 21:44:24 +0000

    - added recursive mkdir in ftps library code.

--- includes/protocol-ftpslibrary.php
+++ includes/protocol-ftpslibrary.php
@@ -10,9 +10,22 @@ if( function_exists( 'ftp_ssl_connect' ) ) {
 	
 	if( $ftp_connection !== FALSE ) {
 		if( @ftp_login( $ftp_connection, $remote_settings['username'], $final_password ) !== FALSE ) {
-			// Make sure the remote directory exists.
-			@ftp_mkdir( $ftp_connection, $final_dir );
+			// Make sure the remote directory exists, ftp_mkdir() doesn't support recursive directories, so do it manually.
+			$parts = explode( '/', $final_dir );
 			
+			// Get the current working directory so we can return to it later.
+			$cwd = ftp_pwd( $ftp_connection );
+			
+			foreach( $parts as $part ) {
+				if( !@ftp_chdir( $ftp_connection, $part ) ) {
+					ftp_mkdir( $ftp_connection, $part );
+					ftp_chdir( $ftp_connection, $part );
+				}
+			}
+			
+			// Go back to where we started.
+			ftp_chdir( $ftp_connection, $cwd );
+
 			$result = @ftp_put( $ftp_connection, $final_dir . $filename, $archive, FTP_BINARY );
 			
 			// If we have been told to send the log file as well, let's do that now.
